772d12d8dc1a30e141525bad205cdae3b56a18dd
Marlon Marques <marlonbr1@gmail.com>
2015-01-31 00:24:13 -0800

    recreates rails app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_asl-game-ror_session'
+Rails.application.config.session_store :cookie_store, key: '_SB-Hacks_session'
